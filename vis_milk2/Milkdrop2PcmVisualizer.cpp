0d63baaab11e9fa2647175dbb9bd2e67445647a5
Se7enSlasher <74858188+Se7enSlasher@users.noreply.github.com>
2022-09-06 15:38:43 +0300

    Modified the starting program window size.

--- vis_milk2/Milkdrop2PcmVisualizer.cpp
+++ vis_milk2/Milkdrop2PcmVisualizer.cpp
@@ -27,8 +27,8 @@
 #define DLL_EXPORT __declspec(dllexport)
 //#define COMPILE_AS_DLL
 #define SAMPLE_SIZE 576
-#define DEFAULT_WIDTH 1280;
-#define DEFAULT_HEIGHT 720;
+#define DEFAULT_WIDTH 640;
+#define DEFAULT_HEIGHT 480;
 
 CPlugin g_plugin;
 HINSTANCE api_orig_hinstance = nullptr;
