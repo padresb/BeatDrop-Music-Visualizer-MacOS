ef6c2d563f619060be8772a442a0618c272fccb0
Incubo_ <74858188+OfficialIncubo@users.noreply.github.com>
2023-12-18 10:16:45 +0200

    One comment added in the b.d. code.

--- vis_milk2/plugin.cpp
+++ vis_milk2/plugin.cpp
@@ -9656,6 +9656,7 @@ for (int i=0;i<576;i++)
       /*int start = MY_FFT_SAMPLES * i / 6;
         int end = MY_FFT_SAMPLES * (i+1) / 6;
         int j;*/ //old MD beat detection algorithm, not accurate, only reacts only at the quarter of middle and treble.
+		//it depends on the sample rate input in your computer, so possibly it's not reacting correctly. See fft.cpp
 
 		mysound.imm[i] = 0;
 
